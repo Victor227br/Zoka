f82c82a9f2ba29f1ac8c447b570f0aaa6b189c64
Victor <victorsouzavieira.22@gmail.com>
2026-06-23 19:01:13 -0300

    fix: trying to debug the form

--- src/components/UseProductForm.jsx
+++ src/components/UseProductForm.jsx
@@ -1,11 +1,7 @@
 import { useState, useEffect } from "react";
-import { setItem } from "../services/ProductService";
+// import { getItem,setItem } from "../services/ProductService";
 
 const UseProductForm = () => {
-
-    function generateIDProduct () {
-        
-    }
 
     const [formData, setFormData] = useState({
         Product: "",
@@ -15,8 +11,18 @@ const UseProductForm = () => {
         Status: "Active ",
         Description: ""
     })
-
+    
     const [products, setProducts] = useState([])
+    // 1-Pegar o objeto e inserir no array
+    // 2-Manter os dados no array e adicionar um novo item no array
+    // 3-Transformar esse array em json
+    // 4-Pegar o json do localstorage, manter os dados salvos e adicionar um novo  
+
+    useEffect(() =>{
+        const getDataProducts = JSON.parse(localStorage.getItem('productsData')) ? JSON.parse(localStorage.getItem('productsData')) : [];
+        setProducts(getDataProducts)
+    }, [])
+
 
     const handleSubmit = (event) => {
         event.preventDefault() 
@@ -24,18 +30,22 @@ const UseProductForm = () => {
             window.alert("Preencha direito")
         } else {
             console.log("Enviado")
-            setProducts((products) => {
-                return [...products, formData]
+            setProducts((products) =>{
+                return[...products, formData]
             })
-        } 
-        setItem(products)
-}
-  
-    return {
+        }
+    }    
+
+        useEffect(() => {
+        localStorage.setItem('productsData', JSON.stringify(products))
+        },[products])
+
+    
+    return {   
         formData,
         setFormData,
         handleSubmit
-    }
+}
 }
 
 export default UseProductForm
