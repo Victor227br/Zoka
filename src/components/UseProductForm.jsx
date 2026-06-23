1d6d591df3e4044fcc1d9177c5ec225f255379ac
Victor <victorsouzavieira.22@gmail.com>
2026-06-23 11:53:40 -0300

    feat: send data form to localstorage

--- src/components/UseProductForm.jsx
+++ src/components/UseProductForm.jsx
@@ -1,6 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { setItem } from "../services/ProductService";
 
 const UseProductForm = () => {
+
+    function generateIDProduct () {
+        
+    }
 
     const [formData, setFormData] = useState({
         Product: "",
@@ -11,13 +16,21 @@ const UseProductForm = () => {
         Description: ""
     })
 
+    const [products, setProducts] = useState([])
+
     const handleSubmit = (event) => {
         event.preventDefault() 
         if (formData.Product.trim() === "" || formData.Price.trim() === "" || formData.Description.trim() === "" || formData.Stock.trim() === ""){
             window.alert("Preencha direito")
+        } else {
+            console.log("Enviado")
+            setProducts((products) => {
+                return [...products, formData]
+            })
         } 
+        setItem(products)
 }
-
+  
     return {
         formData,
         setFormData,
