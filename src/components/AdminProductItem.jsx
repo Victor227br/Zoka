8402b346bd61b2d504213ef274653875b0552b8c
Victor <victorsouzavieira.22@gmail.com>
2026-06-29 22:13:11 -0300

    Recive Props

--- src/components/AdminProductItem.jsx
+++ src/components/AdminProductItem.jsx
@@ -1,36 +1,31 @@
 
-const AdminProductItem = () => {
+const AdminProductItem = ({product}) => {
+
+  const {Product, Price, Stock, Type, Status, Description} = product;
 
   return (
 <div className=" bg-white rounded-xl shadow-sm p-4 border border-slate-100">
   <div className="flex items-center justify-between lg:mr-7 lg:ml-5">
 
     <div className="flex items-center gap-3">
-
       <img className="h-14 w-14 rounded-xl object-cover" src="/src/assets/picture/cappuccino.jpg" alt=""/>
-
       <div className="flex flex-col justify-center gap-1">
-        <h3 className="font-bold text-[#1D3557] text-xs ">Cappuccino</h3>
-        <p className="text-[#0344DC] text-xs font-medium">Hot Drink</p>
-        <p className="text-xs text-slate-500">Price: $9.33</p>
+        <h3 className="font-bold text-[#1D3557] text-xs ">{Product}</h3>
+        <p className="text-[#0344DC] text-xs font-medium">{Type}</p>
+        <p className="text-xs text-slate-500">{Price}</p>
       </div>
-
     </div>
 
-   <div className="flex flex-col ">
-
-
+  <div className="flex flex-col ">
   <div className="flex flex-col items-center">
-    <p className="text-[12px] text-slate-500">Stock</p>
-
-    <h3 className="font-bold text-slate-800">33</h3>
+    <p className="text-[12px] text-slate-500"></p>
+    <h3 className="font-bold text-slate-800">{Stock}</h3>
   </div>
 
   <div className="bg-[#E6FCE7] px-2 rounded-full flex items-center justify-center gap-1 ">
     <div className="w-2 h-2 rounded-full bg-[#22C55E]" />
-    <span className="text-[10px] font-medium text-[#22C55E]">Active</span>
+    <span className="text-[10px] font-medium text-[#22C55E]">{Status}</span>
   </div>
-
 </div>
 
     <div className="flex items-center gap-2 lg:gap-4">
@@ -41,14 +36,11 @@ const AdminProductItem = () => {
       <button className="w-9 h-9 rounded-lg bg-[#FEEAEA] flex items-center justify-center lg:w-10 lg:h-10">
         <img className="h-6" src="/src/assets/icon/icon_trash.png" alt=""/>
       </button>
-
     </div>
-
   </div>
 </div>
 
   );
 };
-
 
 export default AdminProductItem;
