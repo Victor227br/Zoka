6694f54e45d7ea306a2209a757d3451fd1010a32
Victor <victorsouzavieira.22@gmail.com>
2026-07-01 14:59:19 -0300

    feat: pass the function delete for button on the product

--- src/components/AdminProductItem.jsx
+++ src/components/AdminProductItem.jsx
@@ -1,19 +1,17 @@
+const AdminProductItem = ({product, deleteProduct}) => {
 
-const AdminProductItem = ({product}) => {
+  const {id, Product, Price, Stock, Type, Status, Description} = product;
 
-  const {Product, Price, Stock, Type, Status, Description} = product;
-
-  return (
-<div className=" bg-white rounded-xl shadow-sm p-4 border border-slate-100">
-  <div className="flex items-center justify-between lg:mr-7 lg:ml-5">
-
-    <div className="flex items-center gap-3">
-      <img className="h-14 w-14 rounded-xl object-cover" src="/src/assets/picture/cappuccino.jpg" alt=""/>
-      <div className="flex flex-col justify-center gap-1">
-        <h3 className="font-bold text-[#1D3557] text-xs ">{Product}</h3>
-        <p className="text-[#0344DC] text-xs font-medium">{Type}</p>
-        <p className="text-xs text-slate-500">{Price}</p>
-      </div>
+return (
+  <div className=" bg-white rounded-xl shadow-sm p-4 border border-slate-100">
+    <div className="flex items-center justify-between lg:mr-7 lg:ml-5">
+      <div className="flex items-center gap-3">
+        <img className="h-14 w-14 rounded-xl object-cover" src="/src/assets/picture/cappuccino.jpg" alt=""/>
+        <div className="flex flex-col justify-center gap-1">
+          <h3 className="font-bold text-[#1D3557] text-xs ">{Product}</h3>
+          <p className="text-[#0344DC] text-xs font-medium">{Type}</p>
+          <p className="text-xs text-slate-500">{Price}</p>
+       </div>
     </div>
 
   <div className="flex flex-col ">
@@ -29,11 +27,15 @@ const AdminProductItem = ({product}) => {
 </div>
 
     <div className="flex items-center gap-2 lg:gap-4">
-     <button className="w-9 h-9 rounded-lg bg-[#EEF4FF] flex items-center justify-center lg:w-10 lg:h-10">
+     <button
+     onClick={() => editProduct(id)} 
+      className="w-9 h-9 rounded-lg bg-[#EEF4FF] flex items-center justify-center lg:w-10 lg:h-10">
         <img className="h-7" src="/src/assets/icon/icon_edit.png"alt=""/>
       </button>
 
-      <button className="w-9 h-9 rounded-lg bg-[#FEEAEA] flex items-center justify-center lg:w-10 lg:h-10">
+      <button
+      onClick={() => deleteProduct(id)}
+       className="w-9 h-9 rounded-lg bg-[#FEEAEA] flex items-center justify-center lg:w-10 lg:h-10">
         <img className="h-6" src="/src/assets/icon/icon_trash.png" alt=""/>
       </button>
     </div>
