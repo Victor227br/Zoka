31ec1976ae21b1b850c357473cf9056ca9d5e71d
Victor <victorsouzavieira.22@gmail.com>
2026-07-21 23:27:26 -0300

    refactor: adding the JSON to localStorage when it is empty

--- src/services/ProductService.jsx
+++ src/services/ProductService.jsx
@@ -1,7 +1,16 @@
-export const getItem = () =>{
-    return JSON.parse(localStorage.getItem("productsData")) || [];
-}
+import productsData from "../ProductsData.json";
+
+export const getItem = () => {
+  const getProduct = localStorage.getItem("productsData");
+  if (!getProduct) {
+    localStorage.setItem("productsData", JSON.stringify(productsData));
+    return productsData;
+  } else {
+    return JSON.parse(getProduct);
+  }
+};
 
 export const setItem = (dataProducts) => {
-    localStorage.setItem('productsData', JSON.stringify(dataProducts))
-}
+  localStorage.setItem("productsData",JSON.stringify(dataProducts));
+};
+
