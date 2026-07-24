6096b25689128be0b766a6a5c50bf895388eef6b
Victor <victorsouzavieira.22@gmail.com>
2026-07-24 18:44:12 -0300

    style: improving and making it responsive the style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,151 +1,175 @@
 import MenuCategories from "../components/MenuCategories";
+import { Carousel } from "../components/carouselHome";
 
 const Home = () => {
-    return (
+
+  let slides = [
+      "/src/assets/picture/coffee_package-carousel.png",
+      "/src/assets/picture/cappuccino.jpg",
+      "/src/assets/picture/coffee_package-carousel.png",
+      "/src/assets/picture/cappuccino.jpg",
+  ]
+
+  return (
     <>
-    <section 
+  <section
     className="
     relative
     w-full
     min-h-screen
-    bg-[url(/src/assets/picture/image-hero.jpg)]
+    bg-[url('/src/assets/picture/hero_zoka_mobile.png')]
+    lg:bg-[url('/src/assets/picture/hero_zoka_desktop-img.png')]
     bg-cover
     bg-no-repeat
-    bg-[center_top]
+    bg-[50%_10%]
     lg:bg-center">
 
-        <header 
-        className=" 
-        flex 
-        items-center 
-        justify-between 
-        h-[9vh] 
-        bg-black/40 
-        backdrop-blur-md 
-        border-b 
-        border-white/10">
-
-            <h1 className="ml-6 text-2xl text-white lg:ml-16 ">Zoka</h1>
-            <div className=" flex items-center justify-around w-24 mr-8 lg:mr-16">
-
-                <div className="relative flex items-center justify-center cursor-pointer">
-                    <div className="absolute -top-2 -right-2 flex items-center justify-center w-5 h-5 rounded-full bg-[#0344DC]">
-                        <p className="text-xs text-white">2</p>
-                    </div>
-                        <img className="h-7 cursor-pointer" src="./src/assets/icon/icon_shoppingCart-white.png"/>
-                </div>
-
+      <header 
+      className=" 
+      flex 
+      items-center 
+      justify-between 
+      h-[9vh] 
+      bg-black/40 
+      backdrop-blur-md 
+      border-b 
+      border-white/10">
+        
+        <h1 className="ml-6 text-[clamp(1.5rem,1.5vw,2.3rem)] text-white lg:ml-16">Zoka</h1>
+          <div className=" flex items-center justify-around w-24 mr-8 lg:mr-16">
+            <div className="relative flex items-center justify-center cursor-pointer">
+              <div className="absolute -top-2 -right-2 flex items-center justify-center w-5 h-5 rounded-full bg-[#0344DC]">
+                <p className="text-xs text-white">2</p>
+              </div>
+              
+                <img className="h-7 cursor-pointer" src="./src/assets/icon/icon_shoppingCart-white.png"/>
+              </div>
                 <div className="cursor-pointer">
                     <img className="h-9" src="./src/assets/icon/icon_profile.png" alt="Profile"/>
                 </div>
             </div>
         </header>
 
-            <div className="
-                sm:flex
-                justify-center
-                w-screen
-                min-h-screen
-                bg-gradient-to-t
-                from-black/90
-                via-black/20
-                to-transparent
-                bg-blend-multiply
-                lg:h-[92%]">
+<div
+  className="
+    flex
+    flex-col
+    items-center
+    justify-center
+    w-full
+    min-h-screen
+    bg-gradient-to-t
+    from-black/90
+    via-black/20
+    to-transparent
+    pb-[70%]
+    bg-blend-multiply
+    lg:items-start
+    lg:justify-center
+    lg:pb-[10%]
+    lg:pl-[10%]">
+      
+  <div
+    className="
+      w-[90%]
+      max-w-md
+      flex
+      flex-col
+      text-center
+      items-center
+      lg:max-w-none
+      lg:w-[42%]
+      lg:items-start
+      lg:text-left">
+        
+  <h1 className="text-white font-light leading-none text-[clamp(3.5rem,6vw,6.5rem)]">Zoka</h1>
+    <div className="mt-6 leading-[0.95]">
+      <h2 className="text-white font-medium text-[clamp(2rem,3.4vw,4rem)]">Strong <span className="text-blue-500 font-medium">Coffee</span></h2>
+      <h2 className="text-white lg:font-thin lg:tracking-wide lg:font-extralight text-[clamp(2rem,3.4vw,4rem)]">Clean <span className="text-blue-500 ">Experience</span></h2>
+     
+      <div className="flex items-center  mr-6 ml-6 text-center lg:m-0">
+      <p className="text-white mt-6 lg:ml-1">Freshly roasted specialty coffee with bold flavor and unforgettable aroma.</p>
+      </div>
 
-                <div className=" flex flex-col items-center justify-around w-[80%] h-[42vh] m-6">
-                 <h1 className="text-white text-[clamp(4rem,4.5vw,14.6rem)] font-bold-400 mt-16">Zoka</h1>
+    </div>
 
-                        <div className="mt-4 text-center">
-                            <h2 className="text-white text-center text-[clamp(2rem,3.2vw,12.6rem)]">Strong <span className="text-blue-300">Coffee</span></h2>
-                            <h2 className="text-white text-center text-[clamp(2rem,3.2vw,12.6rem)]">Clean <span className="text-blue-300">Experience</span></h2>
-                        </div>
+    <button
+      className="
+        mt-8
+        flex
+        items-center
+        justify-center
+        w-[60%]
+        bg-[#0344DC]
+        text-white
+        h-8
+        rounded-2xl
+        shadow-lg
+        shadow-[0_0_35px_rgba(3,68,220,0.45)]
+        duration-300
+        hover:scale-105
+        hover:bg-white/30
+        hover:text-white
+        lg:w-[46%]
+        lg:h-[4.2vh]
+        lg:text-[1.2vw]
+        lg:bg-white
+        lg:text-[#0344DC]">
+      Explore Menu 
+    </button>
+    
+  </div>
+</div>
 
-                    <button 
-                    className="
-                    flex
-                    items-center
-                    justify-center
-                    w-[54%]
-                    p-1
-                    mt-8
-                    text-[16px]
-                    text-blue-700
-                    bg-white
-                    border
-                    border-white/20
-                    rounded-2xl
-                    shadow-lg
-                    backdrop-blur-lg
-                    duration-300
-                    hover:bg-white/30
-                    hover:text-white
-                    hover:scale-105
-                    lg:w-[16vw]">
-                     Explore Menu 
-                     </button>
+</section>
+   <div className="flex flex-col items-center justify-center mt-20 px-6">
+  <p
+    className="
+      max-w-4xl
+      text-center
+      text-[#2F2F2F]
+      text-sm
+      leading-8
+      font-normal
+      md:text-md
+      lg:text-md"> 
+      Since <span className="font-semibold">1912</span>, Zoka in Santos has been
+    part of Brazil's <span className="font-semibold">coffee history and culture</span>.
+    We are an in-house roasting and grinding company where
+    <span className="font-semibold"> tradition and quality</span> go hand in hand,
+    and <span className="font-semibold">every bean is treated with care</span>,
+    respecting the coffee's natural timing and processing.
+  </p>
+  <div className="w-[100%] mt-4 h-px bg-gray-900 lg:w-[54%] lg:mt-6"></div>
+</div>
+  
+  <section 
+    className="
+    flex 
+    flex-col 
+    ml-[6%] 
+    mr-[6%] 
+    mt-[4%] 
+    items-center
+    justify-between
+    lg:flex-row
+    lg:items-start">
 
-                    <div className="flex mt-7 gap-x-4">
-                        <div className="
-                            flex
-                            items-center
-                            justify-center
-                            w-12
-                            h-12
-                            border
-                            border-blue-600
-                            rounded-full
-                            cursor-pointer
-                            transition-all
-                            duration-300
-                            hover:bg-blue-600/20
-                            hover:scale-105">
-                             <img className="h-[20px]" src="./src/assets/icon/icon_whatsapp.png" />
-                            </div>
+    <div className="lg:w-[30%] mt-[20%] md:mt-[5%] lg:mt-[10%]">
+      <h2 className="text-[clamp(1.5rem,2vw,2.4rem)] text-[#2F2F2F] font-semibold ">Discover our coffees.</h2>
+      <p className=" text-[#2F2F2F] mt-4">discover our premium coffee collection, crafted from carefully selected beans to deliver rich aroma, exceptional flavor, and an unforgettable coffee experience.</p>
+    </div>
+  <div className="lg:w-[60%] m-auto mt-[10%] md:mt-[5%] lg:mt-0">
+    <Carousel slides={slides}/>
+  </div>
+      {/* <nav className=" h-[8vh] sticky top-0 z-50 -mt-6 bg-white w-[92%] mx-auto rounded-2xl shadow-lg p-4"></nav> */}
+  </section>
 
-                            <div className="
-                                flex
-                                items-center
-                                justify-center
-                                w-12
-                                h-12
-                                border
-                                border-blue-600
-                                rounded-full
-                                cursor-pointer
-                                transition-all
-                                duration-300
-                                hover:bg-blue-600/20
-                                hover:scale-105">
+          <MenuCategories />
 
-                                <img className="h-[20px]" src="./src/assets/icon/icon_instagram.png"/>
-                            </div>
-                        </div>
-
-                        <div className="
-                            flex
-                            items-center
-                            gap-2
-                            p-2
-                            mt-8
-                            border
-                            border-blue-600
-                            rounded-2xl
-                            bg-blue-600/20">
-
-                            <img className="h-[22px]" src="./src/assets/icon/icon_local.png"/>
-                            <p className="text-white sm:text-[14px]"> 333 N Michigan Ave Ste 333</p>
-
-                        </div>
-                    </div>
-                </div>
-            </section>
-        <nav className=" h-[8vh] sticky top-0 z-50 -mt-6 bg-white w-[92%] mx-auto rounded-2xl shadow-lg p-4"></nav>
-            <MenuCategories />
 
     <footer className="bg-[#0F172A] text-white mt-20">
       <section className="max-w-7xl mx-auto px-6 py-14 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
-
         <div>
           <h2 className="text-3xl font-bold text-[#0344DC]">Zoka</h2>
           <p className="text-slate-300 mt-4 leading-7">Fresh coffee, premium quality and unforgettable moments.Enjoy handcrafted drinks prepared with passion.</p>
@@ -166,7 +190,7 @@ const Home = () => {
             <li>
               <a href="#" className="hover:text-[#0344DC] transition">About</a>
             </li>
-
+            
             <li>
               <a href="#" className="hover:text-[#0344DC] transition">Contact</a>
             </li>
@@ -191,14 +215,21 @@ const Home = () => {
             <li>Sunday: 09:00 - 18:00</li>
           </ul>
         </div>
-
       </section>
 
       <div className="border-t border-slate-700">
-        <section className="max-w-7xl mx-auto px-6 py-6 flex flex-col md:flex-row items-center justify-between gap-4">
-
+        <section className="
+        max-w-7xl
+        mx-auto
+        px-6 
+        py-6 
+        flex 
+        flex-col 
+        md:flex-row 
+        items-center 
+        justify-between 
+        gap-4">
           <p className="text-slate-400 text-center md:text-left">© 2026 Zoka Coffee. All rights reserved.</p>
-
           <div className="flex gap-4">
             <a href="#"
               className="
@@ -244,7 +275,7 @@ const Home = () => {
         </section>
       </div>
     </footer>
-        </>
+  </>
     );
 };
 
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,4 +1,3 @@
-import ProductSizeButton from "../components/ProductSizeButton";
 
 const Product = () => {
    
@@ -9,7 +8,6 @@ return(
   </div>
 
   <div className="bg-white rounded-t-[40px] -mt-10 relative z-10 p-8 min-h-[65vh]">
-
     <div>
         <h1 className="text-5xl font-bold text-[#1D3557]">Coffee Brazilian</h1>
         <p className="text-gray-400 mt-3 text-lg">The traditional Brazilian Coffee</p>
@@ -18,26 +16,168 @@ return(
     <div className="mt-4 flex flex-row gap-4">
         <div className="w-32 flex flex-row justify-center items-center h-9 bg-[#E1E9F9] gap-2 rounded-2xl">
             <img  className="h-6 pb-1"  src="./src/assets/icon/icon_hot-coffee.png" alt="" />
-            <h2 className="text-[#1D3557] text-[clamp(0.8rem,1vw,1.4rem)] font-bold">Cold Coffee</h2>
+            <h2 className="text-[#1D3557] text-[clamp(0.7rem,0.8vw,1.2rem)] font-bold">Cold Coffee</h2>
         </div>
           <div className="w-32 flex flex-row justify-center items-center h-9 bg-[#E1E9F9] gap-3 rounded-2xl">
             <img className="h-6"  src="./src/assets/icon/icon_grain-coffee-blue.png" alt="" />
-            <h2 className="text-[#1D3557] text-[clamp(0.8rem,1vw,1.4rem)] font-bold">Brazilian</h2>
+            <h2 className="text-[#1D3557] text-[clamp(0.7rem,0.8vw,1.2rem)]  font-bold">Brazilian</h2>
         </div>
     </div>
         <div className="mt-4">
             <div className="w-[100%] h-[1px] bg-[#1D3557]"></div>
-            <h2 className="text-[#1D3557] text-[clamp(1.2rem,1.3vw,1.8rem)] m-2 font-bold">Size </h2>
+            <h2 className="text-[#1D3557] text-[clamp(1.2rem,1.3vw,1.8rem)] m-2 font-bold">Size</h2>
             <div className="flex flex-row justify-around mt-5 mb-5">
-               <ProductSizeButton size={"110ml"}/>
-               <ProductSizeButton size={"225ml"}/>
-               <ProductSizeButton size={"333ml"}/>
-            </div>    
+      <button 
+        className="
+        text-[#1D3557] 
+        text-[clamp(0.8rem,1vw,1.3rem)]
+        w-24
+        h-9 
+        bg-[#E1E9F9] 
+        flex items-center 
+        justify-center 
+        rounded-2xl 
+        flex-col 
+        cursor-pointer 
+        m-0 
+        active:bg-[#0344DC] 
+        active:text-white 
+        transition duration-150">
+          120ml
+        </button>
+
+      <button 
+        className="
+        text-[#1D3557] 
+        text-[clamp(0.8rem,1vw,1.3rem)]
+        w-24
+        h-9 
+        bg-[#E1E9F9] 
+        flex items-center 
+        justify-center 
+        rounded-2xl 
+        flex-col 
+        cursor-pointer 
+        m-0 
+        active:bg-[#0344DC] 
+        active:text-white 
+        transition duration-150">
+          180ml
+        </button>
+
+      <button 
+        className="
+        text-[#1D3557] 
+        text-[clamp(0.8rem,1vw,1.3rem)]
+        w-24
+        h-9 
+        bg-[#E1E9F9] 
+        flex items-center 
+        justify-center 
+        rounded-2xl 
+        flex-col 
+        cursor-pointer 
+        m-0 
+        active:bg-[#0344DC] 
+        active:text-white 
+        transition duration-150">
+          240ml
+        </button>
+
+
+        </div>    
             <div className="w-[100%] h-[1px] bg-[#1D3557]"></div>
-        </div>
-        <nav>
-           
-        </nav>
+      </div>
+
+  <div
+  className="
+    fixed
+    bottom-0
+    left-0
+    right-0
+    bg-white
+    border-t
+    border-slate-200
+    p-5
+    flex
+    items-center
+    gap-4
+    shadow-[0_-5px_20px_rgba(0,0,0,0.08)]">
+
+    <div
+      className="
+      flex
+      items-center
+      bg-[#E1E9F9]
+      rounded-2xl
+      overflow-hidden">
+
+    <button
+      className="
+        w-12
+        h-12
+        text-2xl
+        font-bold
+        text-[#1D3557]
+        hover:bg-[#D2DDF7]
+        transition">
+      -
+    </button>
+
+    <span
+      className="
+        w-12
+        text-center
+        text-lg
+        font-semibold
+        text-[#1D3557]">
+      1
+    </span>
+
+    <button
+      className="
+        w-12
+        h-12
+        text-2xl
+        font-bold
+        text-[#1D3557]
+        hover:bg-[#D2DDF7]
+        transition">
+      +
+    </button>
+  </div>
+
+  <div className="flex flex-1 gap-3">
+    <button
+      className="
+        flex-1
+        h-12
+        rounded-xl
+        border-2
+        border-[#0344DC]
+        text-[#0344DC]
+        font-semibold
+        hover:bg-[#E1E9F9]
+        transition">
+      Add Cart
+    </button>
+
+    <button
+      className="
+        flex-1
+        h-12
+        rounded-xl
+        bg-[#0344DC]
+        text-white
+        font-semibold
+        hover:bg-[#0236B3]
+        transition">
+      Buy Now
+    </button>
+  </div>
+</div>
+
+
   </div>
 </div>  
 
