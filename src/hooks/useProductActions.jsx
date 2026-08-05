31d589b0c4c1a1a66525600a23a814313efc2133
Victor <victorsouzavieira.22@gmail.com>
2026-08-04 22:54:36 -0300

    refactor: separete logic of filter products

--- src/hooks/useProductActions.jsx
+++ src/hooks/useProductActions.jsx
@@ -1,15 +1,12 @@
-import { useState, useEffect, useReducer} from "react";
+import { useState, useEffect, useReducer, createContext} from "react";
 import { getItem, setItem } from "../services/ProductService";
 import FormProduct from "../components/FormProduct";
 
 export const useProductsActions = () => {
+
   const [isFormOpen, setIsFormOpen ] = useState(false)
   const [products, setProducts] = useState(getItem())
   const [editingProduct, setEditingProduct] = useState(null)
-
-  const [search, setSearch] = useState("");
-  const [category, setCategory] = useState("All");
-  const [status, setStatus] = useState("All");
 
   const openForm = (id) => {
     const findProduct = products.find(product => product.id === id)
@@ -41,45 +38,15 @@ export const useProductsActions = () => {
       product.id === updatedProduct.id ? updatedProduct : product))
   }
 
-  const filteredProducts = products.filter((product) => {
-    const searchFilter = search === "" ||product.name.toLowerCase().includes(search.toLowerCase());
-    const categoryFilter = category === "" || category === "All" || product.type === category;
-    const statusFilter = status === "" || status === "All" || product.status === status;
-      return searchFilter && categoryFilter && statusFilter
-  })
-
-  const clearFilter = () => {
-  setSearch("");
-  setCategory("All");
-  setStatus("All");
-};
-
-  const totalProducts = () => {
-  return products.length;
-  };
-
-  const totalStock = products.reduce((total, product) => {
-  return total + product.stock;
-  }, 0);
-
   return {
     products,
     editingProduct,
     isFormOpen,
-    search,
-    category, 
-    status,
-    filteredProducts,
-    totalStock,
     addProduct,
     removeProduct,
-    totalProducts,
     editProduct,
     openForm, 
     closeForm,
-    clearFilter,
-    setSearch,
-    setCategory,
-    setStatus,
+  
   }
 }
